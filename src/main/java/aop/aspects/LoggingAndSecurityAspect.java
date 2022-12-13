2993c15099d8072c87b50b79827c0c6db865ca95
svenka <svenkagrip@gmail.com>
2022-12-13 21:16:04 +0800

    add beforeGetSecurityAdvice

--- src/main/java/aop/aspects/LoggingAndSecurityAspect.java
+++ src/main/java/aop/aspects/LoggingAndSecurityAspect.java
@@ -2,6 +2,7 @@ package aop.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 
@@ -9,14 +10,18 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class LoggingAndSecurityAspect {
 
+    @Pointcut("execution(*  get*())")
+    private void allGetMethods(){}
+
+
     //@Before("execution(public void aop.UniLibrary.getBook())")
-    @Before("execution(*  get*())")
+    @Before("allGetMethods()")
     public  void beforeGetBookAdvice()
     {
         System.out.println("beforeGetBookAdvice:попытка получить книгу/журнал");
     }
 
-    @Before("execution(*  get*())")
+    @Before("allGetMethods()")
     public  void beforeGetSecurityAdvice()
     {
         System.out.println("beforeGetSecurityAdvice:попытка проверки прав на книгу/журнал");
